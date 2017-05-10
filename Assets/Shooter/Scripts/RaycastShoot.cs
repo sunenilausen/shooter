c84bb57b89da3f4dc975df2a693cc756cb0eb9ae
Sune Sylvest Nilausen <Sune Sylvest Nilausen>
2017-05-10 19:14:30 +0200

    lektion1 færdig

---
+++ Assets/Shooter/Scripts/RaycastShoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour {
+
+	public Transform gunEnd;
+	private Camera fpsCam;
+
+	// Use this for initialization
+	void Start () {
+		fpsCam = GetComponentInParent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Shoot();
+		}
+	}
+
+	void Shoot () {
+		Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0.0f));
+
+		RaycastHit hit;
+
+		if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit)) {
+			hit.transform.gameObject.SetActive (false);
+		}
+	}
+
+}
