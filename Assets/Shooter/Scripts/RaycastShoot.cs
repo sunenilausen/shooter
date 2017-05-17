e253c05e38641dd7f6d170a9a48d447931e0a769
Sune Sylvest Nilausen <Sune Sylvest Nilausen>
2017-05-17 19:01:59 +0200

    færdig lektion2

--- Assets/Shooter/Scripts/RaycastShoot.cs
+++ Assets/Shooter/Scripts/RaycastShoot.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class RaycastShoot : MonoBehaviour {
 
 	public Transform gunEnd;
+	private LineRenderer laserLine;
 	private Camera fpsCam;
+	public int weaponRange = 50;
+	public WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
+	private ParticleSystem gunParticles;
+	public GameObject hitObject;
 
 	// Use this for initialization
 	void Start () {
 		fpsCam = GetComponentInParent<Camera> ();
+		laserLine = GetComponent<LineRenderer> ();
+		gunParticles = gunEnd.GetComponent<ParticleSystem> ();
 	}
 	
 	// Update is called once per frame
@@ -20,13 +27,37 @@ public class RaycastShoot : MonoBehaviour {
 	}
 
 	void Shoot () {
-		Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0.0f));
+		gunParticles.Emit (50);
+
+		Vector3 rayOrigin = gunEnd.position;
 
 		RaycastHit hit;
 
+		laserLine.SetPosition (0, gunEnd.position);
+
 		if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit)) {
-			hit.transform.gameObject.SetActive (false);
+			laserLine.SetPosition (1, hit.point);
+
+			GameObject newObject = Instantiate (hitObject);
+			newObject.transform.position = hit.point;
+			newObject.GetComponent<ParticleSystem> ().Emit(50);
+			
+			if (hit.transform.gameObject.tag == "Shootable") {
+				hit.transform.gameObject.SetActive (false);
+			}
+				
 		}
+		else {
+			laserLine.SetPosition (1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+		}
+
+		StartCoroutine (ShotEffect ());
 	}
 
+	IEnumerator ShotEffect() {
+		laserLine.enabled = true;
+		yield return shotDuration;
+
+		laserLine.enabled = false;
+	}
 }
