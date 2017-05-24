e19b9aa718e52b297eb72246e731f8f66149dc1c
sune nilausen <sunenilausen@gmail.com>
2017-05-24 18:28:41 +0200

    so far

---
+++ Assets/Shooter/Scripts/EnemyHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+	int health;
+	public int maxHealth; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
